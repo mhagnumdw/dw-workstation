2ad981d8b8c0af67f78e3c9c1d6b483a37a100e1
Dwouglas Mhagnum <dwouglas@gmail.com>
2023-10-09 22:14:38 -0300

    Exemplo de script que não precisa do JBang instalado

---
+++ dw.java
@@ -0,0 +1,12 @@
+///usr/bin/env curl -Ls https://sh.jbang.dev | bash -s - "$0" "$@" ; exit $?
+
+// Esse é um exemplo que nem precisa o JBang instalado inicialmente!!!
+
+import static java.lang.System.*;
+
+public class dw {
+
+    public static void main(String... args) {
+        out.println("Hello World");
+    }
+}
